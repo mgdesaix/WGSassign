47bc125de5b22801eef0713692914723a139beb8
mgdesaix <mgdesaix@gmail.com>
2022-09-27 14:30:47 -0600

    fixed tabs/spaces

--- WGSassign/glassy.py
+++ WGSassign/glassy.py
@@ -67,15 +67,15 @@ def loo(L, A, IDs, t, maf_iter, maf_tole):
         
         L1 = pop_index_except * 2
         L2 = L1 + 1
-	      L_cat = np.concatenate((L1, L2))
-	      L_cat_index = np.sort(L_cat, axis = 0).reshape(-1)
-	      L_pop = np.ascontiguousarray(L[:,L_cat_index])
-	      f_pop = shared.emMAF(L_pop, maf_iter, maf_tole, t)
-	      # column index of the allele frequency file (based on unique order of pops)
-	      pop_col = np.argwhere(pops == i_pop)[0,0]
-	      # put new allele frequencies in column
-	      A[:,pop_col] = f_pop
-	      del L_pop, f_pop
+        L_cat = np.concatenate((L1, L2))
+        L_cat_index = np.sort(L_cat, axis = 0).reshape(-1)
+        L_pop = np.ascontiguousarray(L[:,L_cat_index])
+        f_pop = shared.emMAF(L_pop, maf_iter, maf_tole, t)
+        # column index of the allele frequency file (based on unique order of pops)
+        pop_col = np.argwhere(pops == i_pop)[0,0]
+        # put new allele frequencies in column
+        A[:,pop_col] = f_pop
+        del L_pop, f_pop
         
         for j in range(k):
             # set log like vector for individual i to pop j
