d47b81401bd994fa7900e0f64faa608022cb51e4
mgdesaix <mgdesaix@gmail.com>
2023-03-29 19:30:00 -0600

    change L_keep to speed things up

--- WGSassign/zscore.py
+++ WGSassign/zscore.py
@@ -48,54 +48,53 @@ def get_L_keep(L, AD, AD_summary_dict, n_threshold, t, i):
       else:
         L_keep[s] = 1
         loci_kept += 1
-  return L_keep, loci_kept
+  L_keep_final = np.argwhere(L_keep == 1).reshape(-1)
+  return L_keep_final, loci_kept
 
 def get_expected_W_l(L, L_keep, A, AD, AD_summary_dict, t, i, k):
   W_l_obs = 0
-  W_l = np.zeros(AD.shape[0], dtype = np.float32)
+  W_l = np.zeros(L_keep.shape[0], dtype = np.float32)
   e = 0.01
-  for s in np.arange(AD.shape[0]):
-    if L_keep[s] == 1:
-      A_sk = A[s,k]
-      P_gl = [(1-A_sk)*(1-A_sk), 2*A_sk*(1-A_sk), A_sk*A_sk]
-      f_gl = [L[s,2*i] * P_gl[0],L[s,2*i+1] * P_gl[1],(1-L[s,2*i]-L[s,2*i+1]) * P_gl[2]]
-      f_gl_log = np.log(f_gl[0] + f_gl[1] + f_gl[2])
-      W_l_obs += f_gl_log
-      key = str([AD[s,2*i], AD[s,2*i+1]])
-      # Getting the total depth
-      Dl = AD[s,2*i] + AD[s,2*i+1]
-      for Aa in np.arange(Dl+1):
-        Ar = Dl - Aa
-        # A = (Ar, Aa)
-        iter_key = str([Ar, Aa])
-        # iter_key = tuple([Ar, Aa])
-        # Probability of read depths, combinatorial
-        ad_factorial = np.math.factorial(Dl) / (np.math.factorial(Aa)*np.math.factorial(Ar))
-        P_r_a = [ad_factorial*((1-e)**Ar)*(e**Aa), ad_factorial*((1/2)**Dl), ad_factorial*((1-e)**Aa)*(e**Ar)]
-        for j in range(3):
-          W_l[s] += f_gl_log * P_gl[j] * P_r_a[j] * 1 * AD_summary_dict[iter_key][1][j]
+  for s in range(L_keep.shape[0]):
+    A_sk = A[s,k]
+    P_gl = [(1-A_sk)*(1-A_sk), 2*A_sk*(1-A_sk), A_sk*A_sk]
+    f_gl = [L[s,2*i] * P_gl[0],L[s,2*i+1] * P_gl[1],(1-L[s,2*i]-L[s,2*i+1]) * P_gl[2]]
+    f_gl_log = np.log(f_gl[0] + f_gl[1] + f_gl[2])
+    W_l_obs += f_gl_log
+    key = str([AD[s,2*i], AD[s,2*i+1]])
+    # Getting the total depth
+    Dl = AD[s,2*i] + AD[s,2*i+1]
+    for Aa in np.arange(Dl+1):
+      Ar = Dl - Aa
+      # A = (Ar, Aa)
+      iter_key = str([Ar, Aa])
+      # iter_key = tuple([Ar, Aa])
+      # Probability of read depths, combinatorial
+      ad_factorial = np.math.factorial(Dl) / (np.math.factorial(Aa)*np.math.factorial(Ar))
+      P_r_a = [ad_factorial*((1-e)**Ar)*(e**Aa), ad_factorial*((1/2)**Dl), ad_factorial*((1-e)**Aa)*(e**Ar)]
+      for j in range(3):
+        W_l[s] += f_gl_log * P_gl[j] * P_r_a[j] * 1 * AD_summary_dict[iter_key][1][j]
   return W_l_obs, W_l
 
 def get_var_W_l(L, L_keep, A, AD, AD_summary_dict, W_l, t, i, k):
-  var_W_l = np.zeros(AD.shape[0], dtype = np.float32)
+  var_W_l = np.zeros(L_keep.shape[0], dtype = np.float32)
   e = 0.01
-  for s in np.arange(AD.shape[0]):
-    if L_keep[s] == 1:
-      A_sk = A[s,k]
-      P_gl = [(1-A_sk)*(1-A_sk), 2*A_sk*(1-A_sk), A_sk*A_sk]
-      f_gl = [L[s,2*i] * P_gl[0],L[s,2*i+1] * P_gl[1],(1-L[s,2*i]-L[s,2*i+1]) * P_gl[2]]
-      f_gl_log = np.log(f_gl[0] + f_gl[1] + f_gl[2])
-      key = str([AD[s,2*i], AD[s,2*i+1]])
-      # Getting the total depth
-      Dl = AD[s,2*i] + AD[s,2*i+1]
-      for Aa in np.arange(Dl+1):
-        Ar = Dl - Aa
-        # A = (Ar, Aa)
-        iter_key = str([Ar, Aa])
-        # iter_key = tuple([Ar, Aa])
-        # Probability of read depths, combinatorial
-        ad_factorial = np.math.factorial(Dl) / (np.math.factorial(Aa)*np.math.factorial(Ar))
-        P_r_a = [ad_factorial*((1-e)**Ar)*(e**Aa), ad_factorial*((1/2)**Dl), ad_factorial*((1-e)**Aa)*(e**Ar)]
-        for j in range(3):
-          var_W_l[s] += (W_l[s]-f_gl_log)**2 * P_gl[j] * P_r_a[j] * 1 * AD_summary_dict[iter_key][1][j]
+  for s in range(L_keep.shape[0]):
+    A_sk = A[s,k]
+    P_gl = [(1-A_sk)*(1-A_sk), 2*A_sk*(1-A_sk), A_sk*A_sk]
+    f_gl = [L[s,2*i] * P_gl[0],L[s,2*i+1] * P_gl[1],(1-L[s,2*i]-L[s,2*i+1]) * P_gl[2]]
+    f_gl_log = np.log(f_gl[0] + f_gl[1] + f_gl[2])
+    key = str([AD[s,2*i], AD[s,2*i+1]])
+    # Getting the total depth
+    Dl = AD[s,2*i] + AD[s,2*i+1]
+    for Aa in np.arange(Dl+1):
+      Ar = Dl - Aa
+      # A = (Ar, Aa)
+      iter_key = str([Ar, Aa])
+      # iter_key = tuple([Ar, Aa])
+      # Probability of read depths, combinatorial
+      ad_factorial = np.math.factorial(Dl) / (np.math.factorial(Aa)*np.math.factorial(Ar))
+      P_r_a = [ad_factorial*((1-e)**Ar)*(e**Aa), ad_factorial*((1/2)**Dl), ad_factorial*((1-e)**Aa)*(e**Ar)]
+      for j in range(3):
+        var_W_l[s] += (W_l[s]-f_gl_log)**2 * P_gl[j] * P_r_a[j] * 1 * AD_summary_dict[iter_key][1][j]
   return var_W_l
--- WGSassign/zscore_cy.pyx
+++ WGSassign/zscore_cy.pyx
@@ -3,19 +3,41 @@ import numpy as np
 cimport numpy as np
 from cython.parallel import prange
 from cython import boundscheck, wraparound
+from libc.math cimport log
 
 # Calculate zscore
 # L is the reader_cy read beagle file, matrix M x (2 * N)
-cpdef AD_summary(float[:,::1] AD, float[:,::1] L, int t, int i):
+cpdef expected_W_l(float[:,::1] L, float[::1] L_keep, float[:,::1] A, float[:,::1] AD, dict AD_summary_dict, int t, int i, int k, float W_l_obs, float[::1] W_l, float e):
     cdef int m = L.shape[0]
-    cdef int s
-    cdef dict AD_GL_dict = {}
-    cdef float like0, like1, like2
+    cdef int s, Dl, Aa, Ar
+    cdef float A_sk, P_gl0, P_gl1, P_gl2, f_gl0, f_gl1, f_gl2, f_gl_log, ad_factorial, P_r_a0, P_r_a1, P_r_a2
+    cdef str key, iter_key
     with nogil:
             for s in prange(m, num_threads=t):
-                key = tuple(AD[s,2*i], AD[s,2*i+1])
-                if key not in AD_GL_dict.keys():
-                    AD_GL_dict[key] = [[L[s,2*i], L[s,2*i+1], (1 - L[s,2*i] - L[s,2*i+1])]]
-                else:
-                    AD_GL_dict[key].append([L[s,2*i], L[s,2*i+1], (1 - L[s,2*i] - L[s,2*i+1])])
-    return AD_GL_dict
+                if L_keep[s] == 1:
+                    A_sk = A[s,k]
+                    P_gl0 = (1-A_sk)*(1-A_sk)
+                    P_gl1 = 2*(1-A_sk)*A_sk
+                    P_gl2 = A_sk*A_sk
+                    f_gl0 = L[s,2*i] * P_gl0
+                    f_gl1 = L[s,2*i+1] * P_gl1
+                    f_gl2 = (1-L[s,2*i]-L[s,2*i+1]) * P_gl2
+                    f_gl_log = log(f_gl0 + f_gl1 + f_gl2)
+                    W_l_obs = W_l_obs + f_gl_log
+                    key = str([AD[s,2*i], AD[s,2*i+1]])
+                    Dl = AD[s,2*i] + AD[s,2*i+1]
+                    for Aa in range(Dl+1):
+                        Ar = Dl - Aa
+                        iter_key = str([Ar, Aa])
+                        ad_factorial = np.math.factorial(Dl) / (np.math.factorial(Aa)*np.math.factorial(Ar))
+                        P_r_a0 = ad_factorial*((1.0-e)**Ar)*(e**Aa)
+                        P_r_a1 = ad_factorial*((0.5)**Dl)
+                        P_r_a2 = ad_factorial*((1.0-e)**Aa)*(e**Ar)
+                        W_l[s] = W_l[s] + f_gl_log * P_gl0 * P_r_a0 * AD_summary_dict[iter_key][1][0]
+                        W_l[s] = W_l[s] + f_gl_log * P_gl1 * P_r_a1 * AD_summary_dict[iter_key][1][1]
+                        W_l[s] = W_l[s] + f_gl_log * P_gl2 * P_r_a2 * AD_summary_dict[iter_key][1][2]
+                    
+                    
+                    
+                    
+                    
